f0f284a39e38442aea89ecfe8e7278a034ee615f
Krystian Koper <krystiankoper@SBVPN205.local>
2018-01-30 16:29:27 +0100

    Added electron to project

---
+++ public/electron.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
+
+const devPath = 'http://localhost:3000';
+const prodPath = `file://${path.join(__dirname, '../build/index.html')}`;
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadURL(isDev ? devPath : prodPath);
+  mainWindow.on('closed', () => mainWindow = null);
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
